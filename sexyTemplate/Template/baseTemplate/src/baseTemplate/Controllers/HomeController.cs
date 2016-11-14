e138054991dbe82cee94769d76b0dce134d8bfd4
JacobAhlberg <jacahlberg@hotmail.com>
2016-11-14 11:15:21 +0100

    TEMPLATE READY

---
+++ sexyTemplate/Template/baseTemplate/src/baseTemplate/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baseTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int age, string user)
+        {
+            String text = user + " is " + age + " years old"; 
+            return View((object)text);
+        }
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult SendingData(String text)
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
