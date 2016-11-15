155ec413c42760cf4306201895591e9c1b93953b
JacobAhlberg <jacahlberg@hotmail.com>
2016-11-15 11:43:08 +0100

    jquery

--- sexyTemplate/Template/baseTemplate/src/baseTemplate/wwwroot/js/site.js
+++ sexyTemplate/Template/baseTemplate/src/baseTemplate/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿
+﻿var Game = function () {
+    var width = document.getElementById('#grid-size');
+    alert(width);
+
+
+
+};
+
+
+
+
+$(document).ready(function () {
+    var game = new Game();
+});
