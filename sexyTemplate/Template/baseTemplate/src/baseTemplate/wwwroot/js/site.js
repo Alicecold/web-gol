2d0b9a1b25585ad36c6f1413fdf5b266d554609c
JacobAhlberg <jacahlberg@hotmail.com>
2016-11-15 12:28:41 +0100

    ä

--- sexyTemplate/Template/baseTemplate/src/baseTemplate/wwwroot/js/site.js
+++ sexyTemplate/Template/baseTemplate/src/baseTemplate/wwwroot/js/site.js
@@ -1,6 +1,12 @@
 ﻿var Game = function () {
     var width = document.getElementById("width").value;
     var height = document.getElementById("height").value;
+
+    $('#grid-size').on(function () {
+        width = document.getElementById("width").value;
+        height = document.getElementById("height").value;
+        alert(height);
+    });
 };
 
 
