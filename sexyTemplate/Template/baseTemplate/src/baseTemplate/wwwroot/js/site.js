5b7cf6472f0a64048366d5db70d44c4295ef2570
JacobAhlberg <jacahlberg@hotmail.com>
2016-11-15 13:19:59 +0100

    lol

--- sexyTemplate/Template/baseTemplate/src/baseTemplate/wwwroot/js/site.js
+++ sexyTemplate/Template/baseTemplate/src/baseTemplate/wwwroot/js/site.js
@@ -1,15 +1,26 @@
 ﻿var Game = function () {
     var width = document.getElementById("width").value;
     var height = document.getElementById("height").value;
+    var column = "", appendRow = "", inc = 1, selectedCells = [], toRemoveClass = [], toAddClass = [], maxValue;
 
     $('#grid-size').focusout(function () {
         width = document.getElementById("width").value;
         height = document.getElementById("height").value;
-        alert(height);
     });
 
 
+    var createGrid = function (height, width) {
 
+        for (var rows = 1; rows <= height; rows++) {
+            for (var col = 1; col <= width; col++) {
+                column += "<td  id =" + inc + col + ">  </td>";
+            }
+            appendRow += "<tr>" + column + "</tr>";
+            column = "";
+            inc++;
+        }
+        $(".table").append(appendRow);
+    };
 };
 
 
